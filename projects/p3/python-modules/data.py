9428a9f58650729b38218fe715b5a811ba486940
Federico Maria Massari <federico.massari@bocconialumni.it>
2017-12-10 18:49:20 +0100

    docs: Add complete docstrings to data.py

--- projects/p3/python-modules/data.py
+++ projects/p3/python-modules/data.py
@@ -3,13 +3,29 @@ Parse the elements in an OpenStreetMap file, transforming them from document
 format to tabular format, and write the data to .csv files, to be imported to
 a SQL database as tables.
 
+The transformation process is as follows:
+ - Use iterparse ('get_element' function) to iteratively step through each top
+   level element in the XML file;
+ - Shape each element into several data structures using the 'shape_element'
+   custom function;
+ - Use a schema ('schema') and a validation library ('cerberus') to ensure the
+   transformed data is in the correct format ('validate_element');
+ - Write each data structure to the appropriate .csv files ('process_map').
+
 Apart from the 'shape_element' function, all code (with minor modifications)
-taken from [1].
+taken from [1]. Input to the 'shape_element' function provided by [2].
+
+Note: Use Python 3 to run this script. Conversion was made using [3].
+
 
 References
 -------------------------------------------------------------------------------
 [1] 'Project: Wrangle OpenStreetMap Data', Data Wrangling Course, Udacity
     Data Analyst Nanodegree
+[2] https://discussions.udacity.com/t/p3-project-combining-auditing-cleaning-
+    and-csv-creation/231037
+[3] https://discussions.udacity.com/t/add-cleaning-functions-to-shape-element
+    /348188/3
 '''
 
 import csv
@@ -54,6 +70,10 @@ WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
 WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
 WAY_NODES_FIELDS = ['id', 'node_id', 'position']
 
+'''
+The 'shape_element' function takes as input an iterparse Element object and
+returns a dictionary.
+'''
 def shape_element(element, node_attr_fields=NODE_FIELDS,
                   way_attr_fields=WAY_FIELDS, problem_chars=PROBLEMCHARS,
                   lower_colon=LOWER_COLON, default_tag_type='regular'):
@@ -63,21 +83,33 @@ def shape_element(element, node_attr_fields=NODE_FIELDS,
     way_nodes = []
     tags = []
 
-
+    # The starting index of the 'nd' tag in 'way_nodes.csv', see below
     position = 0
 
+    # For each child (secondary tag) of <node> or <way>, look for <tag>
     for child in element:
         tag = {}
         if child.tag == 'tag':
 
+            # If tag 'k' value contains problematic characters, ignore it
             if problem_chars.search(child.attrib['k']):
                 continue
 
+            '''
+            If tag 'k' value contains colon, set the character before colon as
+            the tag type, and characters after it as the tag key. If there are
+            additional colons in the 'k' value, ignore and keep as part of the
+            tag key.
+            '''
             elif lower_colon.search(child.attrib['k']):
                 tag['id'] = element.attrib['id']
                 tag['type'] = child.attrib['k'].split(':', 1)[0]
                 tag['key'] = child.attrib['k'].split(':', 1)[1]
 
+                '''
+                Programmatically clean 'street', 'postcode', and 'city' tag
+                values using functions from module clean.py
+                '''
                 if tag['key'] == 'street':
                     for i in range(len(query_types)):
                         child.attrib['v'] = \
@@ -93,10 +125,15 @@ def shape_element(element, node_attr_fields=NODE_FIELDS,
                     tag['value'] = clean.update_city_name(child.attrib['v'],
                                                           re_library[-1])
 
+                # Leave other tag values unmodified
                 else:
                     tag['value'] = child.attrib['v']
 
             else:
+                '''
+                Set tag type to 'regular' (default) if no colon in the tag 'k'
+                value is present.
+                '''
                 tag['type'] = default_tag_type
                 tag['key'] = child.attrib['k']
                 tag['value'] = child.attrib['v']
@@ -105,6 +142,14 @@ def shape_element(element, node_attr_fields=NODE_FIELDS,
             if tag:
                 tags.append(tag)
 
+        '''
+        'way_nodes.csv' holds a list of dictionaries, one for each 'nd' child
+        tag. Each dictionary has the fields:
+         - 'id': the top level element (way) id;
+         - 'node_id': the ref attribute value of the 'nd' tag;
+         - 'position': the index, starting at 0, of the 'nd' tag, i.e. what
+            order the 'nd' tag appears within the way element.
+        '''
         way_node = {}
         if child.tag == 'nd':
             way_node['id'] = element.attrib['id']
@@ -113,23 +158,24 @@ def shape_element(element, node_attr_fields=NODE_FIELDS,
             way_nodes.append(way_node)
             position += 1
 
-
+    '''
+    Return dictionaries for 'nodes.csv', 'nodes_tags.csv', 'ways.csv', and
+    'ways_tags.csv'.
+    '''
     if element.tag == 'node':
-        for label in NODE_FIELDS:
-            node_attribs[label] = element.attrib[label]
+        for field in NODE_FIELDS:
+            node_attribs[field] = element.attrib[field]
         return {'node': node_attribs, 'node_tags': tags}
 
     elif element.tag == 'way':
-        for label in WAY_FIELDS:
-            way_attribs[label] = element.attrib[label]
+        for field in WAY_FIELDS:
+            way_attribs[field] = element.attrib[field]
         return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
 
 
-# ================================================== #
-#               Helper Functions                     #
-# ================================================== #
+# Helper functions
 def get_element(osm_file, tags=('node', 'way', 'relation')):
-    """Yield element if it is the right type of tag"""
+    '''Yield element if it is the right type of tag'''
 
     context = ET.iterparse(osm_file, events=('start', 'end'))
     _, root = next(context)
@@ -138,22 +184,19 @@ def get_element(osm_file, tags=('node', 'way', 'relation')):
             yield elem
             root.clear()
 
-
 def validate_element(element, validator, schema=SCHEMA):
-    """Raise ValidationError if element does not match schema"""
+    '''Raise ValidationError if element does not match schema'''
     if validator.validate(element, schema) is not True:
         field, errors = next(validator.errors.items())
-        message_string = "\nElement of type '{0}' has the following errors:\n{1}"
+        message_string = "\nElement of type '{0}' has following errors:\n{1}"
         error_string = pprint.pformat(errors)
 
         raise Exception(message_string.format(field, error_string))
 
 
-# ================================================== #
-#               Main Function                        #
-# ================================================== #
+# Main function
 def process_map(file_in, validate):
-    """Iteratively process each XML element and write to csv(s)"""
+    '''Iteratively process each XML element and write to csv(s)'''
 
     with codecs.open(NODES_PATH, 'w') as nodes_file, \
          codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
@@ -191,6 +234,8 @@ def process_map(file_in, validate):
 
 
 if __name__ == '__main__':
-    # Note: Validation is ~ 10X slower. For the project consider using a small
-    # sample of the map when validating.
+    '''
+    Note: Validation is ~ 10X slower. For the project consider using a small
+    sample of the map when validating.
+    '''
     process_map(OSM_PATH, validate=False)
