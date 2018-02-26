6242b67a42565ebff9d94aeb909fc2bf2c7beb79
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-02-26 09:05:58 +0100

    feat: Add library and file imports to p4.R

--- projects/p4/R-modules/p4.R
+++ projects/p4/R-modules/p4.R
@@ -1,3 +1,15 @@
 # Run while "R-modules" is the current working directory
 system("python3 ../python-modules/get_xls.py")
 system("python3 ../python-modules/scrape_wikipedia.py")
+
+# Import libraries
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+
+# Import file content as data frames
+greatest_pt1 <- readxl::read_excel("../data/xls/1000GreatestFilms.xls")
+greatest_pt2 <- readxl::read_excel("../data/xls/Films-Ranked-1001-2000.xls")
+gdp <- read.csv("../data/csv/gdp.csv")
+continents <- read.csv("../data/csv/continents.csv")
+world <- map_data("world")
