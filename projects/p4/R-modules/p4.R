8691fdb61b2749e9c75545875318fb03763f4d41
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-02-27 09:17:50 +0100

    feat: Process main data frame in p4.R

--- projects/p4/R-modules/p4.R
+++ projects/p4/R-modules/p4.R
@@ -13,3 +13,20 @@ greatest_pt2 <- readxl::read_excel("../data/xls/Films-Ranked-1001-2000.xls")
 gdp <- read.csv("../data/csv/gdp.csv")
 continents <- read.csv("../data/csv/continents.csv")
 world <- map_data("world")
+
+# Uniform the data frames, append pt2 to pt1
+greatest <- rbind(greatest_pt1[, -c(2:3)], greatest_pt2)
+
+# Remove single files to free up space
+rm(greatest_pt1, greatest_pt2)
+
+# Single-out main country of production
+greatest$region <- stringr::str_split_fixed(greatest$Country, "-", 2)[,1]
+
+# Convert to factors (before: chr)
+world$region <- factor(world$region)
+greatest$region <- factor(greatest$region)
+
+# Add factor levels
+levels(greatest$region) <- c(levels(greatest$region),
+c("China", "Czech Republic", "Serbia"))
