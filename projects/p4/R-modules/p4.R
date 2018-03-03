ae0761769fe16c0a465b18dd5c08fa055a20f1d4
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-03-03 18:02:27 +0100

    feat: Add link to scrape_website.py in p4.R

--- projects/p4/R-modules/p4.R
+++ projects/p4/R-modules/p4.R
@@ -1,5 +1,6 @@
 # Run while "R-modules" is the current working directory
 system("python3 ../python-modules/get_xls.py")
+system("python3 ./python-modules/scrape_webpage.py")
 system("python3 ../python-modules/scrape_wikipedia.py")
 
 # Import libraries
@@ -10,6 +11,7 @@ library(gridExtra)
 # Import file content as data frames
 greatest_pt1 <- readxl::read_excel("../data/xls/1000GreatestFilms.xls")
 greatest_pt2 <- readxl::read_excel("../data/xls/Films-Ranked-1001-2000.xls")
+directors <- read.csv("../data/csv/top_250_directors.csv")
 gdp <- read.csv("../data/csv/gdp.csv")
 continents <- read.csv("../data/csv/continents.csv")
 world <- map_data("world")
